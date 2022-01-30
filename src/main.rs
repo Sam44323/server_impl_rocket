5ea020fe79d129cb97064058e3dea38866600aa2
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-30 23:51:53 +0530

    add: added initializer for JSON

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
 #[macro_use]
 extern crate rocket;
+use rocket::serde::json::Json;
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct Task {
+    description: String,
+    completed: bool,
+}
 
 #[get("/test")]
 fn index() -> &'static str {
     "Testing the server😁"
 }
 
+#[post("/todo", format = "json", data = "<todo>")]
+fn add_todo(todo: Json<Task>) {}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/api", routes![index])
+    rocket::build().mount("/api", routes![index, add_todo])
 }
