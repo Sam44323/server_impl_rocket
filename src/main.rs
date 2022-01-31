49e8baa50cecb1269e6c2166925c956a250a8e64
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-31 21:41:29 +0530

    update: updated the code_structure

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,12 @@ extern crate rocket;
 use rocket::serde::json::Json;
 use serde::Deserialize;
 
-#[derive(Deserialize, Debug)]
-struct Task {
-    description: String,
-    completed: bool,
-}
-
 #[get("/test")]
 fn index() -> &'static str {
     "Testing the server😁"
 }
 
-#[post("/todo", format = "json", data = "<todo>")]
-fn add_todo(todo: Json<Task>) {}
-
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/api", routes![index, add_todo])
+    rocket::build().mount("/", routes![index, add_todo])
 }
