b7188715e745e9313267bde9d4621c3396c7d96c
Suranjan <suranjanbiswas2002@gmail.com>
2022-02-01 01:21:30 +0530

    update: updated the docs for db collection

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -1,7 +1,7 @@
 use rusqlite::*;
 
 pub fn connect_db() {
-  // creating a DB connection
+  // creating a db connection
   let db_connection = Connection::open("data.sqlite").unwrap();
 
   // creating a table for the database
