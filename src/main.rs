cec7548127e54d38c8d70aa63cf25875086452e5
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-31 22:25:00 +0530

    update: updated the code for creating columns in database

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ fn rocket() -> _ {
 
     db_connection
         .execute(
-            "create table if not exists todo_list (id integer primary key)",
+            "create table if not exists todo_list (id integer primary key item varchar(255) no null)",
             [],
         )
         .unwrap();
